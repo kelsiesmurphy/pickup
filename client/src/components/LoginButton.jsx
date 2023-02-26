b9681d465a65e934cc9334f203f0dd72d44b4665
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-02-26 12:35:07 +0000

    adding newsletter

--- client/src/components/LoginButton.jsx
+++ client/src/components/LoginButton.jsx
@@ -6,7 +6,7 @@ const LoginButton = ({ buttonText }) => {
   return (
     <button
       onClick={handleLogin}
-      className="flex md:max-w-[132px] justify-center items-center gap-2 rounded-lg border border-green-800 bg-green-800 py-2.5 px-4 text-white shadow-sm transition-colors hover:border-green-900 hover:bg-green-900"
+      className="flex items-center justify-center gap-2 rounded-lg border border-green-800 bg-green-800 py-2.5 px-4 text-white shadow-sm outline-slate-900 transition-colors hover:border-green-900 hover:bg-green-900 md:max-w-[132px]"
     >
       {buttonText}
     </button>
--- client/src/components/landing/Hero.jsx
+++ client/src/components/landing/Hero.jsx
@@ -3,21 +3,23 @@ import LoginButton from "../LoginButton";
 
 const Hero = () => {
   return (
-    <header className="flex justify-center py-24 border-t">
+    <header className="flex justify-center border-t py-24">
       <div className="flex flex-col gap-16">
         <div className="mx-4 flex flex-col gap-12">
-          <div className="space-y-6 max-w-3xl">
-            <h1 className="text-4xl font-semibold text-slate-900 md:text-6xl transition-all">
+          <div className="max-w-3xl space-y-6">
+            <h1 className="text-4xl font-semibold text-slate-900 transition-all md:text-6xl">
               {HeroConstant.main_heading}
             </h1>
-            <p className="text-lg md:text-xl text-slate-500">{HeroConstant.sub_heading}</p>
+            <p className="text-lg text-slate-500 md:text-xl">
+              {HeroConstant.sub_heading}
+            </p>
           </div>
           <LoginButton buttonText={"Get started"} />
         </div>
         <img
           src={HeroConstant.hero_image}
           width="1248"
-          className="aspect-[375/272] md:aspect-[1216/480] object-cover xl:rounded-2xl"
+          className="aspect-[375/272] object-cover md:aspect-[1216/480] xl:rounded-2xl"
         />
       </div>
     </header>
