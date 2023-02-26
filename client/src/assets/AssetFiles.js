dcbb6f9045d7a7a866587e6c6cf33fe440aae0ca
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-02-26 12:58:09 +0000

    Completion of landing page

--- client/src/assets/AssetFiles.js
+++ client/src/assets/AssetFiles.js
@@ -2,6 +2,14 @@ import wordmark from "./wordmark.png";
 import logomark from "./logomark.svg";
 import image404 from "./image404.jpg";
 import heroImage from "./hero_image.jpg";
-import communityExample from "./community_example.png";
+import communityExampleDesktop from "./communityExampleDesktop.svg";
+import communityExampleMobile from "./communityExampleMobile.svg";
 
-export { wordmark, logomark, image404, heroImage, communityExample };
+export {
+  wordmark,
+  logomark,
+  image404,
+  heroImage,
+  communityExampleDesktop,
+  communityExampleMobile,
+};
--- client/src/components/landing/Statistics.jsx
+++ client/src/components/landing/Statistics.jsx
@@ -13,10 +13,15 @@ const Statistics = () => {
   });
 
   return (
-    <div className="flex flex-col text-center gap-5 py-24 px-4 lg:px-32">
-      <h2 className="text-slate-900 text-4xl font-semibold">We've been growing!</h2>
-      <p className="text-lg text-slate-500">Since we launched in February 2023, we've been helping groups clean up their communities!</p>
-      <ul className="mt-12 flex flex-wrap justify-around gap-8 p-16 bg-green-50 rounded-lg">
+    <div className="flex flex-col gap-5 py-24 px-4 text-center lg:px-32">
+      <h2 className="text-4xl font-semibold text-slate-900">
+        We've been growing!
+      </h2>
+      <p className="text-lg text-slate-500">
+        Since we launched in February 2023, we've been helping groups clean up
+        their communities!
+      </p>
+      <ul className="mt-12 flex flex-wrap justify-around gap-8 rounded-lg bg-green-50 p-16">
         {aboutNodes}
       </ul>
     </div>
