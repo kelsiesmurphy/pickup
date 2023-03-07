5d2b33db73b5b4763edd90105324770c18dc62e1
jomonty <100322748+jomonty@users.noreply.github.com>
2023-03-07 09:13:17 +0000

    add basic auth0 setup

---
+++ server/src/main/java/org/pickup/backend/server/components/Message.java
@@ -0,0 +1,13 @@
+package org.pickup.backend.server.components;
+
+public class Message {
+    private final String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
