45ba8b80154fadc266c8795ee317619237915c1c
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-03-08 22:58:55 +0000

    fixing footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="flex basis-full justify-center pt-16 pb-12">
       <hr />
-      <div className="mx-4 flex flex-1 items-center justify-between border-t py-8 transition-all lg:mx-28">
+      <div className="mx-4 flex flex-wrap  flex-1 items-center justify-center md:justify-between border-t gap-6 py-8 transition-all lg:mx-28">
         <img src={wordmark} className="aspect-[156/48] h-12" />
         <p className="text-slate-500">© 2023 Pickup. All rights reserved.</p>
       </div>
