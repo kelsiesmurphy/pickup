73ae5298e8d93d0f233c4a5a5916280581f8e844
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-03-08 10:32:54 +0000

    adding changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig(
     server: {
       proxy: {
         "/api": {
-          target: "http://localhost:8080",
+          target: "http://139.59.155.108:8080",
           changeOrigin: true,
           secure: false,
           ws: true,
